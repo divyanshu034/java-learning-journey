9be3484638fc648dbc69a2e72f163c9aaab1971d
Divyanshu yaduvanshi <divyanshuyaduvanshi99@gmail.com>
2026-07-12 13:34:08 +0530

    Organize practice set programs
    Moved Java practice set files into a separate folder to keep the repository clean and well organized.
    Day 1 & 2 practice set combine

---
+++ Practice-set/cwh_07_ps1.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class cwh_07_ps1 {
+    static void main(String[] args) {
+//  question 1 summ of 3 number
+//
+//        int a =10 , b = 50 , c = 40 ;
+//        int sum = a + b+ c ;
+//        System.out.println(sum);
+
+//        question 2
+
+//        float s1 = 60 , s2 = 95 , s3 = 70 ;
+//        float cgpa = (s1 + s2 + s3 )/ 30;
+//
+//        System.out.println(cgpa);
+
+//        Question 3
+//Scanner sc = new Scanner(System.in) ;
+//        System.out.println("Enter you'r name :- ");
+//        String str = sc.next() ;
+//
+//        System.out.println("-----GREETINGS FOR YOU--------- ");
+//
+//
+//        System.out.println("Hello " + str + ", Have a good day  ");
+//
+
+//        Question  4
+
+//        double kilometer = 50 ;
+//        double miles = 0.621371 ;
+//
+//        double kmtomile = kilometer * miles ;
+//        System.out.println("Km to miles = " + kmtomile);
+
+
+//        Question 5
+
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Enter you'r  number  :- ");
+
+        System.out.println(sc.hasNextInt());
+
+
+
+
+
+
+
+    }
+}
