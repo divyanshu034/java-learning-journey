a4065942089d108dfb69a2069b30b26b6746533c
Divyanshu yaduvanshi <divyanshuyaduvanshi99@gmail.com>
2026-07-14 18:18:09 +0530

    practice set for loop's

---
+++ Practice-set/Cwh_25_ps5th.java
@@ -0,0 +1,53 @@
+import java.util.Scanner ;
+public class Cwh_25_ps5th {
+    static void main(String[] args) {
+        // question  1
+//        int n = 4 ;
+//        for (int  i = n ; i >0 ; i--  ) {
+//
+//            for (int j=0 ; j < i  ; j++ ){
+//            System.out.print("*");
+//
+//            }
+//            System.out.print("\n");
+//
+//        }
+//}
+        // Question 2
+
+//Scanner sc =new Scanner(System.in) ;
+//        int n = 3 ;
+//        int sum = 0 ;
+//        int i = 0 ;
+//        while (i < n ) {
+//            sum = sum + (2*i) ;
+//
+//            i++ ;
+//        }
+//        System.out.println("sum of 3 even number");
+//        System.out.println(sum);
+//    }
+
+//      Question 3rd
+
+//    Scanner sc = new Scanner(System.in) ;
+//    int n = sc.nextInt();
+//    int table = 1 ;
+//    for ( int i = 1 ; i <= 10 ; i++ )
+//        {
+//           table = (n * i );
+//            System.out.println(n + " * " + i +" = " + table);
+//        }
+
+        Scanner sc = new Scanner(System.in) ;
+    int n = sc.nextInt();
+    int table  ;
+    for ( int i =10  ; i >= 1  ; i-- )
+        {
+           table = (n * i );
+            System.out.println(n + " * " + i +" = " + table);
+        }
+
+    }
+    }
+
